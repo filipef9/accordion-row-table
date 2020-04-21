935e418dfc9cbdafef8b633b27273baf50733182
Filipe dos Santos Nascimento <filipef9@hotmail.com>
2020-04-21 17:07:25 -0300

    Implementação e configuração de converters e de da formatação dos valores
     - Criação de um converter para o tipo LocalDate
     - Formatação da coluna de valor monetário

---
+++ src/main/java/me/fsn/accordion/row/table/converter/LocalDateTimeConverter.java
@@ -0,0 +1,29 @@
+package me.fsn.accordion.row.table.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Faces converter for support of LocalDate
+ * @author Juneau
+ * Code from https://www.javacodegeeks.com/2015/06/utilizing-the-java-8-date-time-api-with-jsf-and-java-ee-7.html
+ */
+@FacesConverter("localDateTimeConverter")
+public class LocalDateTimeConverter implements Converter {
+
+    @Override
+    public Object getAsObject(FacesContext facesContext, UIComponent uiComponent, String value) {
+        return LocalDate.parse(value);
+    }
+
+    @Override
+    public String getAsString(FacesContext facesContext, UIComponent uiComponent, Object value) {
+        final LocalDate dateValue = (LocalDate) value;
+        return dateValue.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    }
+    
+}
