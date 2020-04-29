3e6a1e1228c9110e4dfec0701a94012a3a8227a9
Filipe dos Santos Nascimento <filipef9@hotmail.com>
2020-04-29 15:40:47 -0300

    Partial functional implementation of collapse and expand div.

--- src/main/java/me/fsn/accordion/row/table/model/Transaction.java
+++ src/main/java/me/fsn/accordion/row/table/model/Transaction.java
@@ -1,26 +1,34 @@
 package me.fsn.accordion.row.table.model;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import java.time.LocalDate;
 
 public class Transaction {
 
+    private final Integer id;
     private final LocalDate date;
     private final String description;
     private final Double amount;
 
-    private Transaction(LocalDate date, String description, Double amount) {
+    private Transaction(Integer id, LocalDate date, String description, Double amount) {
+        this.id = id;
         this.date = date;
         this.description = description;
         this.amount = amount;
     }
 
     public static Transaction of(
+        final Integer id,
         final LocalDate date,
         final String description,
         final Double amount
     ) {
         // TODO: implementar validação dos argumentos, antes de criar o objeto.
-        return new Transaction(date, description, amount);
+        return new Transaction(id, date, description, amount);
+    }
+
+    public Integer getId() {
+        return id;
     }
 
     public LocalDate getDate() {
